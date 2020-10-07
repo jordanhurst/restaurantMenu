7be5d1bb6bf1bf48d214adde0b87be7669090349
jordan hurst <jordanhurst@gmail.com>
2020-10-06 22:42:01 -0500

    added MenuItem con and Menu con&methods

--- restaurantMenu/Menu.cs
+++ restaurantMenu/Menu.cs
@@ -1,10 +1,74 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace restaurantMenu
 {
     public class Menu
     {
+        private DateTime TimeStamp { get; private set; } = new DateTime();
+        private List<MenuItem> appetizers = new List<MenuItem>();
+        private List<MenuItem> mains = new List<MenuItem>();
+        private List<MenuItem> desserts = new List<MenuItem>();
+        public List<MenuItem>[] fullMenu = new List<MenuItem>[3];
+        private string lineDivider = "===============================";
+        
         public Menu()
         {
+            this.fullMenu[0] = appetizers;
+            this.fullMenu[1] = mains;
+            this.fullMenu[2] = desserts;
+        }
+
+        public void AddMenuItem(MenuItem item)
+        {
+            if (item.Category == "appetizer")
+            {
+                appetizers.Add(item);
+            } else if (item.Category == "main")
+            {
+                mains.Add(item);
+            }else if (item.Category == "dessert")
+            {
+                desserts.Add(item);
+            }
+        }
+
+        public void DisplayMenu()
+        {
+            Console.WriteLine("Appetizers\n"+ lineDivider)
+            foreach (MenuItem item in appetizers)
+            {
+                string isItNew = "";
+                if ((DateTime.Now - item.dateAdded).TotalDays < 7)
+                {
+                    isItNew = "recently added!";
+                }
+                string itemString = item.Name + "   $" + item.Cost.ToString() + "   " + isItNew + "\n" + item.Description + "\n";
+                Console.WriteLine(itemString);
+            }
+            Console.WriteLine("\n\n" + lineDivider + "Main Entrees\n" + lineDivider);
+            foreach (MenuItem item in mains)
+            {
+                string isItNew = "";
+                if ((DateTime.Now - item.dateAdded).TotalDays < 7)
+                {
+                    isItNew = "recently added!";
+                }
+                string itemString = item.Name + "   $" + item.Cost.ToString() + "   " + isItNew + "\n" + item.Description + "\n";
+                Console.WriteLine(itemString);
+            }
+            Console.WriteLine("\n\n" + lineDivider + "Desserts\n" + lineDivider);
+            foreach (MenuItem item in desserts)
+            {
+                string isItNew = "";
+                if ((DateTime.Now - item.dateAdded).TotalDays < 7)
+                {
+                    isItNew = "recently added!";
+                }
+                string itemString = item.Name + "   $" + item.Cost.ToString() + "   " + isItNew + "\n" + item.Description + "\n";
+                Console.WriteLine(itemString);
+            }
+
         }
     }
 }
--- restaurantMenu/MenuItem.cs
+++ restaurantMenu/MenuItem.cs
@@ -3,8 +3,29 @@ namespace restaurantMenu
 {
     public class MenuItem
     {
-        public MenuItem()
+        public string Name { get; private set; }
+        public double Cost { get; private set; }
+        public string Category
         {
+            get; private set
+            {
+                if ((value != "dessert") || (value != "main") || (value != "appetizer"))
+                {
+                    throw new ArgumentException("category must be 'appetizer' 'main' or 'dessert'");
+                };
+            }
+        }
+        public string Description { get; private set; }
+        public DateTime currentDate = DateTime.Now;
+        internal DateTime dateAdded;
+
+        public MenuItem(string name, double cost, string category, string description)
+        {
+            Name = name;
+            Cost = cost;
+            Category = category;
+            Description = description;
+            DateTime dateAdded = currentDate;
         }
     }
 }
